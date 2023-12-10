beaf032b498782a69e114563454104dd66416da4
GustavoGuerato <guerato.gustavo14@gmail.com>
2023-12-10 18:24:54 -0300

    creation of biblioteca and livro classes

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ class Pessoa:
         self.endereco = endereco
 
     def verifica_cpf(self):
-        novo_cpf = self.cpf[:-2]
+        cpf = input("Digite o CPF: ")
+
+        novo_cpf = cpf[:-2]
         reverso = 10
         total = 0
 
@@ -25,15 +27,48 @@ class Pessoa:
                 total = 0
                 novo_cpf += str(d)
 
-        sequencia = novo_cpf == str(novo_cpf[0]) * len(self.cpf)
+        sequencia = novo_cpf == str(novo_cpf[0]) * len(cpf)
 
-        if self.cpf == novo_cpf and not sequencia:
+        if cpf == novo_cpf and not sequencia:
             print('Válido')
         else:
             print('Inválido')
 
-    def mostra_info(self):
-        print(f'seu CPF é {self.cpf}')
-        print(f'Seu Nome é {self.nome}')
-        print(f'seu endereço é {self.endereco}')
-    
+    def endereco(self):
+        rua = input('Digite o nome de sua rua: ')
+        bairro = input('Digite o seu bairro: ')
+        cidade = input('Digite sua cidade: ')
+
+        endereco_completo = f' o seu endereço é{rua}, {bairro}, {cidade}'
+        return endereco_completo
+
+
+class Biblioteca:
+    livros = []
+
+    def __init__(self, titulo, data_devolucao=None):
+        self.titulo = titulo
+        self.data_devolucao = data_devolucao
+        self.emprestado = False
+
+    def adiciona_livro(self, livro):
+        self.livros.append(livro)
+
+    def listar_livros(self):
+        if not self.livros:
+            print('A nossa biblioteca não tem nenhum livro no momento.')
+        print('Livros disponiveis em nossa biblioteca: ')
+        for livro in self.livros:
+            if livro.esta_disponivel():
+                print(f'{livro.titulo}está disponivel')
+
+class Livro:
+
+    def __init__(self, titulo, autor, data_devolucao=None):
+        self.titulo = titulo
+        self.autor = autor
+        self.data_devolucao = data_devolucao
+        self.emprestado = False
+
+    def realizar_emprestimo(self):
+        pass
